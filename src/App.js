57afa000fa207f8d1cfde33dd7bd12322f30daff
Jayson Su <s86xu@uwaterloo.ca>
2018-03-04 19:36:13 -0500

    Initial Structure
    --Structure in such a way to accomidate react-router-dom
    --Move CSS around and into LESS for node-less-chokidar

--- src/App.js
+++ src/App.js
@@ -1,18 +1,19 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import {BrowserRouter as Router, Route, Redirect, Switch} from 'react-router-dom';
+
+// Components.
+import {Homepage} from './components/homepage/homepage'
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <Router>
+          <Switch>
+            <Redirect exact from='/' to='/Home'/>
+            <Route path='/Home' component={Homepage}/>
+          </Switch>
+        </Router>
       </div>
     );
   }
